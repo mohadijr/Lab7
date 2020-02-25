1711bccd4d72e9e3e4832454992da1680c2eaedc
Mohamed Ali <mali98@student.cscc.edu>
2020-02-25 15:18:23 -0500

    Initial commit

---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,80 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * Sub class
+ * @author Mohamed Ali
+ */
+
+public class RPSLSpock {
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * Boolean method that determines whether human pick is valid or not
+     * @param pick
+     * @return
+     */
+
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * Method that generates computer pick randomly.
+     * @return pick
+     */
+
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     * Method that determines which pick wins, between the computer and human pick
+     * @param c_pick
+     * @param h_pick
+     * @return
+     */
+
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
---
+++ src/edu/cscc/RPSLSpockTest.java
@@ -0,0 +1,45 @@
+package edu.cscc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RPSLSpockTest {
+
+    @Test
+    public void isValidPick() {
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.ROCK));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.PAPER));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.SCISSORS));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.LIZARD));
+        Assert.assertTrue(RPSLSpock.isValidPick(RPSLSpock.SPOCK));
+        Assert.assertFalse(RPSLSpock.isValidPick("school"));
+
+    }
+
+    @Test
+    public void generatePick() {
+        for(int i=0; i<1000000; i++) {
+            String pick = RPSLSpock.generatePick();
+            Assert.assertNotNull(pick);
+            Assert.assertTrue(RPSLSpock.isValidPick(pick));
+        }
+    }
+
+    @Test
+    public void isComputerWin() {
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.ROCK, RPSLSpock.SCISSORS));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.ROCK, RPSLSpock.LIZARD));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.PAPER, RPSLSpock.ROCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.PAPER, RPSLSpock.SPOCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SCISSORS, RPSLSpock.PAPER));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SCISSORS, RPSLSpock.LIZARD));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.LIZARD, RPSLSpock.PAPER));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.LIZARD, RPSLSpock.SPOCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SPOCK, RPSLSpock.ROCK));
+        Assert.assertTrue(RPSLSpock.isComputerWin(RPSLSpock.SPOCK, RPSLSpock.SCISSORS));
+        Assert.assertFalse(RPSLSpock.isComputerWin(RPSLSpock.PAPER, RPSLSpock.SCISSORS));
+
+    }
+}
